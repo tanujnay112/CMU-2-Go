c748b66b0659abc85e83116d9d4f73d182a06750
Abel Tesfaye <abel-tesfaye@hotmail.com>
2017-02-11 04:45:17 -0500

    screens

---
+++ app/src/main/java/c105/com/cmu2go/ConfirmDestinationFragment.java
@@ -0,0 +1,34 @@
+package c105.com.cmu2go;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ConfirmDestinationFragment extends Fragment {
+    private FragmentSwitchListener listener;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            listener = (FragmentSwitchListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + " must implement OnArticleSelectedListener");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.fragment_confirmdest, container, false);
+        return view;
+    }
+}
--- app/src/main/java/c105/com/cmu2go/MainFragment.java
+++ app/src/main/java/c105/com/cmu2go/MainFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -37,7 +38,8 @@ public class MainFragment extends Fragment {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //wait
+                //wait time
+                Toast.makeText(getContext(), "Not implemented", Toast.LENGTH_LONG).show();
             }
         });
         Button button4 = (Button) view.findViewById(R.id.button4);
@@ -51,7 +53,7 @@ public class MainFragment extends Fragment {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //deliver
+                listener.switchFragment(new DeliverFragment());
             }
         });
 
