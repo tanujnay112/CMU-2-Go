52d0302461aa789b0a13e31ab077da96c1fb8cab
Tanuj Nayak <tanujnay112@live.com>
2017-02-11 12:10:27 -0500

    Zoom camera adjustment

--- app/src/main/java/c105/com/cmu2go/MapsActivity.java
+++ app/src/main/java/c105/com/cmu2go/MapsActivity.java
@@ -2,6 +2,7 @@ package c105.com.cmu2go;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
@@ -9,11 +10,14 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -62,12 +66,30 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         } else {
             LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Location location = lm.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
             double longitude = location.getLongitude();
             double latitude = location.getLatitude();
 
-            LatLng sydney = new LatLng(-latitude, longitude);
+            LatLng sydney = new LatLng(latitude, longitude);
             mMap.addMarker(new MarkerOptions().position(sydney).title("Your location"));
+            CameraUpdate center=
+                    CameraUpdateFactory.newLatLng(new LatLng(40.76793169992044,
+                            -73.98180484771729));
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            Criteria criteria = new Criteria();
+
+            if (location != null)
+            {
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 13));
+
+                CameraPosition cameraPosition = new CameraPosition.Builder()
+                        .target(new LatLng(location.getLatitude(), location.getLongitude()))      // Sets the center of the map to location user
+                        .zoom(100)                   // Sets the zoom
+                        .bearing(90)                // Sets the orientation of the camera to east
+                        .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                        .build();                   // Creates a CameraPosition from the builder
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
             mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         }
     }
